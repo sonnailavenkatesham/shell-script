cf36508552fa1a578d2f0d58b4f903370fa3ff08
Roman Reings <Rajkumar>
2024-02-16 08:58:21 +0530

    added files

--- catalogue.sh
+++ catalogue.sh
@@ -2,6 +2,7 @@
 DATE=$(date +%x-%T)
 USER_ID=$(id -u)
 USER=$(id roboshop)
+FILE=/tmp/catalogue/logs
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
@@ -22,14 +23,14 @@ VALIDATE(){
     fi
 }
 
-dnf module disable nodejs -y
+dnf module disable nodejs -y >> $FILE
 VALIDATE $? "module disable nodejs"
 
-dnf module enable nodejs:18 -y
+dnf module enable nodejs:18 -y >> $FILE
 VALIDATE $? "module enable nodejs:18"
 
 
-dnf install nodejs -y
+dnf install nodejs -y >> $FILE
 VALIDATE $? "install nodejs"
 
 if [ $USER -ne 0 ]
@@ -52,7 +53,7 @@ if [ -f /tmp/catalogue.zip ]   # For file "if [ -f /home/rama/file ]"
      echo -e "$Y catalogue.zip dir present $N"
  else
      echo -e "$G catalogue.zip Download the application code $N"
-     curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip
+     curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip >> $FILE
 fi
 
 cd /app 
@@ -63,12 +64,12 @@ if [ -d /app/schema ]
      echo -e "$Y data present $N"
  else
      echo -e "$G "unzinping catalogue" $N"
-     unzip /tmp/catalogue.zip
+     unzip /tmp/catalogue.zip >> $FILE
 fi
 # unzip /tmp/catalogue.zip
 # VALIDATE $? "unzinping catalogue"
 
-npm install 
+npm install >> $FILE
 VALIDATE $? "npm install "
 
 cp /home/centos/shell-script/catalogue.service /etc/systemd/system/catalogue.service
@@ -86,7 +87,7 @@ VALIDATE $? "start catalogue"
 cp /home/centos/shell-script/mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "copying mongo.repo"
 
-yum install mongodb-org-shell -y
+yum install mongodb-org-shell -y >> $FILE
 VALIDATE $? "install mongodb-org-shell"
 
 mongo --host mongodb.venkateshamsonnalia143.online < /app/schema/catalogue.js
